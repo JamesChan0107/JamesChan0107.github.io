964b796e62cb5000cca1fa1e265c37e2f24b96d1
JamesChan0107 <167755144+JamesChan0107@users.noreply.github.com>
2024-06-18 22:08:39 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+let currentPage = 1;
+let allDataLoaded = false;
+
+async function fetchContestData(page) {
+    try {
+        const response = await fetch(`https://memes.tw/wtf/api?contest=621&page=${page}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error fetching data: ${error}`);
+    }
+}
+
+async function loadPage(page) {
+    const data = await fetchContestData(page);
+    if (data && data.length > 0) {
+        clearDataContainer();
+        renderData(data);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+}
+
+function clearDataContainer() {
+    const dataContainer = document.getElementById('container');
+    dataContainer.innerHTML = ''; 
+}
+
+function renderData(data) {
+    const dataContainer = document.getElementById('container');
+
+    data.forEach((item) => {
+        const dataItem = document.createElement('div');
+        dataItem.className = 'dataItem';
+
+        const title = document.createElement('h2');
+        title.textContent = `梗圖title:${item.title}`;
+
+        const link = document.createElement('a');
+        link.href = item.url;
+        link.textContent = '原文連結';
+        link.target = '_blank';
+
+        const image = document.createElement('img');
+        image.src = item.src;
+        image.alt = item.title;
+
+        dataItem.appendChild(title);
+        dataItem.appendChild(link);
+        dataItem.appendChild(image);
+        dataContainer.appendChild(dataItem);
+    });
+}
+
+document.getElementById('fetch-btn').addEventListener('click', () => {
+    currentPage = 1;
+    allDataLoaded = false;
+    loadPage(currentPage);
+    document.getElementById('next-btn').style.display = 'block';
+    document.getElementById('prev-btn').style.display = 'block';
+    document.getElementById('pn-btn-container').style.display = 'flex';
+});
+
+document.getElementById('next-btn').addEventListener('click', () => {
+    currentPage++;
+    loadPage(currentPage).then(() => {
+        checkButtons();
+    });
+});
+
+document.getElementById('prev-btn').addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        loadPage(currentPage).then(() => {
+            checkButtons();
+        });
+    }
+});
+
+function checkButtons() {
+    document.getElementById('prev-btn').style.display = currentPage > 1 ? 'block' : 'none';
+    document.getElementById('next-btn').style.display = allDataLoaded ? 'none' : 'block';
+}
